d1e63d90b7b6d75bc564a572eb1f1d32ec730666
Johanna <johanna.didion@gmail.com>
2020-04-21 19:35:40 +0200

    MOD: added cases to reward function

--- SnakeAI/src/Zischelbot/Zischelbot.java
+++ SnakeAI/src/Zischelbot/Zischelbot.java
@@ -84,11 +84,15 @@ public class Zischelbot implements Bot {
             board.player.moveTo(action, grow);
         }
 
-        private int reward_for_board(Board board){
+        private double reward_for_board(Board board){
             if(board.player.getHead().equals(board.apple))
                 return 1;
             else if(is_dead(board))
                 return -1;
+            else if(board.player.getHead().equals(board.opponent.getHead()))
+                return -0.1;
+            else if(board.opponent.getHead().equals(board.apple))
+                return -0.5;
             return 0;
         }
 
@@ -104,13 +108,12 @@ public class Zischelbot implements Bot {
                 is_head = false;
             }
             //collided with opponent
+            is_head = true;
             for(Coordinate c : board.opponent.body){
-                if(c.equals(head))
+                if(c.equals(head) && !is_head)
                     return true;
+                is_head = false;
             }
-            //collided with opponents head
-            if(head.equals(board.opponent.getHead()))
-                return true;
             return false;
         }
 
@@ -160,7 +163,7 @@ public class Zischelbot implements Bot {
             }
         }
 
-        public int rollout(){
+        public double rollout(){
             int turn = this.turn;
             Board board = this.root;
             while(reward_for_board(board) == 0){
@@ -177,7 +180,7 @@ public class Zischelbot implements Bot {
             return reward_for_board(board);
         }
 
-        public void backpropagation(int reward){
+        public void backpropagation(double reward){
             Game_Tree current = this;
             while(current != null){
                 current.visited += 1;
@@ -215,7 +218,7 @@ public class Zischelbot implements Bot {
         while(System.currentTimeMillis() - start < 80000){
             Game_Tree current = root.selection();
             if(current.visited == 0){
-                int result = current.rollout();
+                double result = current.rollout();
                 current.backpropagation(result);
             }
             else{
@@ -223,7 +226,7 @@ public class Zischelbot implements Bot {
                 if(tmp != null){
                     current = tmp;
                 }
-                int result = current.rollout();
+                double result = current.rollout();
                 current.backpropagation(result);
             }
         }
